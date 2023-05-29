83e72a44b637e0cc65c815fd3c894bf2c1dd2302
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-05-29 23:28:43 +0300

    task : test ci/cd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
    <div>
-    Page
+   <h2 className="text-center bg-skin-accent text-white">The Site you have been looking for ?</h2>
    </div>
   )
 }
