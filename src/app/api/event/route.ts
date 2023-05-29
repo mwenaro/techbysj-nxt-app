85a63ca5785ddd8474ea2cd0dce21cf177a7ff1f
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-05-29 09:12:31 +0300

    fix(ci) : bug-fix in ci/cd

---
+++ src/app/api/event/route.ts
@@ -0,0 +1,49 @@
+import { getData, ref,db, saveData, get } from "@/libs/firebase"
+import { push, set } from "firebase/database";
+import { NextResponse } from "next/server"
+
+export async function GET(request: Request) {
+
+ const callBack = (payload:{data?:any, error?:any}) =>{
+  let result=  payload.data?  ({data:payload.data}) 
+    : {error:payload.error};
+    return NextResponse.json(result)
+ }
+
+
+
+let resp = await getData("events", callBack)
+return NextResponse.json(resp)
+try {
+    const eventRef = ref(db, "events");
+    let snapshot =  await get(eventRef);
+    let data = await snapshot.val()
+        
+    return new Response(JSON.stringify(data),{status:200, statusText:"OK"})
+} catch (error:any) {
+return new  Response (JSON.stringify({error:error.message}),{
+status:500, statusText:"An error occured"
+})    
+}
+
+  }
+
+
+export async function POST(request: Request) {
+try {
+  let body = await request.json()
+let dbRef = ref(db, "events");
+let id = await push(dbRef)
+let _new:any = id.toString().split('/').pop()
+let res = await set(id, {...body, id:_new})
+
+return NextResponse.json({body, id, _id:_new})
+} catch (error:any) {
+  console.log({error:error.message})
+  NextResponse.json({error:error.message})
+return false;  
+}
+
+
+
+}
---
+++ src/libs/firebase.ts
@@ -0,0 +1,105 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getDatabase,
+  set,
+  get,
+  update,
+  remove,
+  ref,
+  child,
+  onValue,
+} from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1bNo7UVZxFniE6uu0eJHqTU8Bc4n10rk",
+  authDomain: "techbysj-app.firebaseapp.com",
+  projectId: "techbysj-app",
+  storageBucket: "techbysj-app.appspot.com",
+  messagingSenderId: "964510738443",
+  appId: "1:964510738443:web:e5cdba76c6e14d7699ff0e"
+};
+
+
+
+const app = initializeApp(firebaseConfig)
+
+const db = getDatabase(app);
+export {
+  getDatabase,
+  set,
+  get,
+  update,
+  remove,
+  ref,
+  child,
+  onValue,
+  db
+}
+
+export function saveData(table: string, data: any) {
+  const id = new Date().getTime();
+  set(ref(db, `${table}/${id}`), data)
+    .then((snapshot) => {
+      console.log({ snapshot });
+      console.log(table + " Successfully created");
+    })
+    .catch((error) => {
+      console.log({ error });
+      console.log("Something went wrong, check data and try again");
+    });
+}
+
+
+export async function updateData(table: string, data: any, id: string | number) {
+  // update(ref(db, `${table}/${id}`), data)
+  //   .then(() => console.log(table + " Successfully created"))
+  //   .catch((error) => {
+  //     console.log("Something went wrong, check data and try again");
+  //   });
+  try {
+    let result = await update(ref(db, `${table}/${id}`), data)
+
+    console.log({ result })
+    return true;
+  } catch (error) {
+    console.log({ error })
+    return false;
+  }
+
+
+}
+
+export async function deleteData(table: string, id: string | number) {
+
+  try {
+    let res = await remove(ref(db, `${table}/${id}`));
+    console.log({ deleteRes: res })
+    return true;
+  } catch (error) {
+    return false;
+  }
+
+
+}
+
+
+export async function getDataById(table: string, id: number | string) {
+  const path = `${table}/${id}`;
+  return await getData(path);
+}
+
+export async function getData(table: string, cb: any = null) {
+  try {
+    const eventRef = ref(db, table);
+    let snapshot = await get(eventRef);
+    let data = await snapshot.val()
+    return { data };
+    // 
+  } catch (error: any) {
+    return { error: error.message }
+  }
+}
